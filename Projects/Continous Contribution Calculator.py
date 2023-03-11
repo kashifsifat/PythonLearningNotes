e961c5a361bd55204cb40b9e7ffabc529cca875b
Sifat Kashif <81516849+kashifsifat@users.noreply.github.com>
2023-03-12 00:29:27 +0600

    Add files via upload

---
+++ Projects/Continous Contribution Calculator.py
@@ -0,0 +1,18 @@
+"""
+Let's imagine a situation where a person helps at least two persons.
+Then each of that two persons help another two persons and it continues...
+How large the figure may become after it hapens for few steps?
+What might be the sum of people if it spreads as three per persons in every stages?
+Let's try to calculate...
+"""
+
+per = int(input("Per person helps: "))
+steps = int(input("Steps/stages continues: "))
+
+total = 0
+
+for i in range (1, steps+1):
+    k = per ** i
+    total += k
+
+print (total)
